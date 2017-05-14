26db7b5f3a7fae63d818deab3e571e47dbcdee8c
liammoroney <liam.moroney14@gmail.com>
2017-05-14 19:11:10 +0100

    Added an arm to the player, when the player mines a block, the arm will play the mining animation. Player can now only mine blocks close to themselves.

--- Assets/Scripts/Modify.cs
+++ Assets/Scripts/Modify.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Modify : MonoBehaviour
 {
     public Camera camera;
+    public Animator anim;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (anim)
+                anim.SetTrigger("Mine");
+               // anim.Play("Mining");
             RaycastHit hit;
             //if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
-            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 100))
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 5))
             {
                 EditTerrain.SetBlock(hit, new BlockAir(), false, true);
             }
