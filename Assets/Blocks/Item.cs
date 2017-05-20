69d57beaee545e1e622eb1624c10f56883769492
liammoroney <liam.moroney14@gmail.com>
2017-05-20 19:11:35 +0100

    Added a script to the item. Fixed 'you are trying to create a monobehaviour using the new keyword', simply by removing the monobehaviour inheritance on the meshdata script. Item contains a boolean which will become true when the item has been alive in the world for over 1 second, once true, the item can be picked up by a player, this stops the item immediately being picked up by the player when they drop the item.

---
+++ Assets/Blocks/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool isReady = false;
+
+    void Start()
+    {
+        StartCoroutine(SetLanded());
+    }
+
+    private IEnumerator SetLanded()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isReady = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && isReady)
+        {
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/MeshData.cs
+++ Assets/Scripts/MeshData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeshData : MonoBehaviour {
+public class MeshData {
 
     public List<Vector3> vertices = new List<Vector3>();
     public List<int> triangles = new List<int>();
